fcd67d5cafb924a12cd66175629eea0765ababd9
Vladimir Petrov <petrov-vm@dartit.ru>
2020-04-19 15:10:33 +0500

    work

--- Sources/App/Helper/Access.swift
+++ Sources/App/Helper/Access.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+enum Access: Int {
+    
+    case modifyCategory = 0b1000000
+    case modifyAward = 0b0100000
+    case adminAward = 0b0010000
+    case manageUserRole = 0b0001000
+    
+    func available(_ value: Int) -> Bool {
+        value & self.rawValue != 0
+    }
+}
