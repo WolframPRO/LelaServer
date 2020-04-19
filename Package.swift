88e1f472eebb0e29c379225cec108db4fa344f34
Vladimir Petrov <petrov-vm@dartit.ru>
2020-04-19 14:40:16 +0500

    5.1.0

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.1.3
+// swift-tools-version:5.1.0
 import PackageDescription
 
 let package = Package(
     name: "LelaServer",
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
         
         // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
         .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
