eec6c070cb739c404e535f7acea7e6c6408bf5ca
Vladimir Petrov <vladimir.petrov@uula.io>
2020-06-14 04:04:41 +0500

    replace get to post

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,7 +4,7 @@ import Vapor
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
     
-    router.get("teapot") {_ in
+    router.post("teapot") {_ in
         return HTTPResponse(status: .custom(code: 418, reasonPhrase: "I’m a teapot"))
     }
     
@@ -25,62 +25,62 @@ public func routes(_ router: Router) throws {
     let userRouter = bearer.grouped("user")
     userRouter.put("change/sudo",        use: userController.sudoChange)
     userRouter.put("change",        use: userController.change)
-    userRouter.get("index",         use: userController.index)
-    userRouter.get("list",          use: userController.list)
+    userRouter.post("index",         use: userController.index)
+    userRouter.post("list",          use: userController.list)
     
     let categoryRouter = bearer.grouped("category")
     let categoryController = CategoryController()
-    categoryRouter.get("list",      use: categoryController.list)
+    categoryRouter.post("list",      use: categoryController.list)
     categoryRouter.post("create",   use: categoryController.create)
     categoryRouter.delete("delete", use: categoryController.delete)
     
     let eventRouter = bearer.grouped("event")
     let eventController = EventController()
-    eventRouter.get("list",         use: eventController.list)
-    eventRouter.get("index",        use: eventController.index)
+    eventRouter.post("list",         use: eventController.list)
+    eventRouter.post("index",        use: eventController.index)
     eventRouter.post("create",      use: eventController.create)
     eventRouter.put("change",       use: eventController.change)
     eventRouter.delete("delete",    use: eventController.delete)
     
     let noteRouter = bearer.grouped("note")
     let noteController = NoteController()
-    noteRouter.get("list",          use: noteController.list)
-    noteRouter.get("index",         use: noteController.index)
+    noteRouter.post("list",          use: noteController.list)
+    noteRouter.post("index",         use: noteController.index)
     noteRouter.post("create",       use: noteController.create)
     noteRouter.put("change",        use: noteController.change)
     noteRouter.delete("delete",     use: noteController.delete)
     
     let partRouter = bearer.grouped("part")
     let partController = PartController()
-    partRouter.get("list",          use: partController.list)
-    partRouter.get("index",         use: partController.index)
+    partRouter.post("list",          use: partController.list)
+    partRouter.post("index",         use: partController.index)
     partRouter.post("create",       use: partController.create)
     partRouter.put("confirm",       use: partController.confirm)
     partRouter.delete("delete",     use: partController.delete)
     
     let awardRouter = bearer.grouped("award")
     let awardController = AwardController()
-   awardRouter.get("list",      use: awardController.list)
+   awardRouter.post("list",      use: awardController.list)
    awardRouter.post("create",   use: awardController.create)
    awardRouter.delete("delete", use: awardController.delete)
-   awardRouter.get("index", use: awardController.index)
+   awardRouter.post("index", use: awardController.index)
    awardRouter.put("change", use: awardController.change)
     
     let awardOfficeRouter = awardRouter.grouped("office")
     let awardOfficeController = AwardOfficeController()
-    awardOfficeRouter.get("list",      use: awardOfficeController.list)
+    awardOfficeRouter.post("list",      use: awardOfficeController.list)
     awardOfficeRouter.post("create",   use: awardOfficeController.create)
     awardOfficeRouter.delete("delete", use: awardOfficeController.delete)
-    awardOfficeRouter.get("index", use: awardOfficeController.index)
+    awardOfficeRouter.post("index", use: awardOfficeController.index)
     awardOfficeRouter.put("change", use: awardOfficeController.change)
     
     
     let awardTypeRouter = awardRouter.grouped("type")
     let awardTypeController = AwardTypeController()
-    awardTypeRouter.get("list",      use: awardTypeController.list)
+    awardTypeRouter.post("list",      use: awardTypeController.list)
     awardTypeRouter.post("create",   use: awardTypeController.create)
     awardTypeRouter.delete("delete", use: awardTypeController.delete)
-    awardTypeRouter.get("index", use: awardTypeController.index)
+    awardTypeRouter.post("index", use: awardTypeController.index)
     awardTypeRouter.put("change", use: awardTypeController.change)
     
     
